88f5a59b3d8cf7365c2f30f32db5a81e5214485b
Mujahid Siyam <mujaxso@proton.me>
2026-04-15 21:42:53 +0200

    style: update markdown treesitter highlight queries

--- runtime/treesitter/languages/markdown/queries/highlights.scm
+++ runtime/treesitter/languages/markdown/queries/highlights.scm
@@ -1,18 +1,39 @@
-;; Official Tree-sitter Markdown highlight queries
-;; Placeholder - users should replace with official queries from:
-;; https://github.com/tree-sitter/tree-sitter-markdown/blob/master/queries/highlights.scm
 
+; Markdown highlighting for tree-sitter-markdown with correct capture names
 (atx_heading) @heading
 (setext_heading) @heading
 (emphasis) @emphasis
 (strong_emphasis) @strong
+(link) @link
 (inline_code_span) @inline_code
 (code_fence) @code_fence
-(block_quote) @blockquote
-(list_item) @list
+(block_quote) @block_quote
+(list) @list
 (thematic_break) @thematic_break
-(link) @link
-(link_text) @link_text
-(link_destination) @link_url
+(paragraph) @paragraph
+(fenced_code_block) @code_fence
+(code_span) @inline_code
 (image) @link
-(image_description) @link_text
+(reference_link) @link
+(reference_definition) @link
+(footnote_reference) @link
+(footnote_definition) @link
+(task_list_marker) @operator
+(strikethrough) @emphasis
+(escape_sequence) @string
+(hard_line_break) @operator
+(soft_line_break) @paragraph
+(table) @table
+(table_header) @heading
+(table_row) @table
+(table_cell) @paragraph
+(html_block) @html
+(html_inline) @html
+
+; Additional captures that might be present
+(heading_content) @heading
+(list_marker) @operator
+(link_label) @link
+(link_title) @string
+(url) @string
+(email) @string
