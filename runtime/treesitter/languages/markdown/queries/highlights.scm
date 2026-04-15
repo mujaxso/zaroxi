6a2f273579d3dd35ce3b757dfecdef58ff64dc42
Mujahid Siyam <mujaxso@proton.me>
2026-04-15 21:57:29 +0200

    fix: replace invalid markdown query with empty one

--- runtime/treesitter/languages/markdown/queries/highlights.scm
+++ runtime/treesitter/languages/markdown/queries/highlights.scm
@@ -1,12 +1,3 @@
 
-; Minimal markdown highlighting query
-; Using wildcard patterns that should work with any grammar
-
-; Match any node - this will always compile
-(_) @plain
-
-; Try to match paragraph if it exists
-(paragraph) @paragraph
-
-; The query will compile even if paragraph doesn't exist
-; because (_) matches everything
+; Empty query for markdown - will compile without errors
+; We'll add patterns once we know the correct node types
